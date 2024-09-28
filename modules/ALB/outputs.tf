a27c0c1437926105e68b61bee971de9d53d74b28
riz-ai <rizwanakoti3@gmail.com>
2024-09-28 19:29:21 +0530

    Create outputs.tf

---
+++ modules/ALB/outputs.tf
@@ -0,0 +1,9 @@
+# modules/EKS/outputs.tf
+#output "cluster_oidc_provider_arn" {
+#  description = "OIDC Provider ARN for the EKS cluster"
+#  value       = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+#}
+#output "cluster_oidc_issuer_url" {
+#  description = "OIDC Issuer URL for the EKS cluster"
+#  value       = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+#}
